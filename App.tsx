f0212926796fb5861279c6a28edaf0e2dee967c1
Brent Vatne <brentvatne@gmail.com>
2021-10-12 14:51:20 -0700

    example

--- App.tsx
+++ App.tsx
@@ -13,11 +13,15 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
+import formatDuration from "format-duration";
 
 const Blue = "#4375a7";
 const HighlightBlue = "#39f";
 const Gray = "#999";
 const Colors = { Blue, HighlightBlue, Gray };
+const FONT_SIZE = 25;
+const MILES_PER_METER = 0.0006213709999999999;
+type DistanceUnit = "km" | "mi";
 
 function Logo() {
   return (
@@ -62,40 +66,43 @@ function Introduction() {
   );
 }
 
-function DistanceInput({ onChangeDistance }) {
+// TODO: handle invalid states instead of depending on this. ux is awkward when
+// you delete things and then end up with a 0 and have to type to have it
+// overwrite the 0
+function parseIntOrZero(text) {
+  const value = parseInt(text, 10);
+  return value ? value : 0;
+}
+
+// TODO: implement onChangeUnit via bottom sheet
+function DistanceInput({ onChangeDistance, unit, onChangeUnit }) {
   const [integer, setInteger] = useState(42);
   const [decimal, setDecimal] = useState(20);
-  const [unit, setUnit] = useState("km");
 
   React.useEffect(() => {
-    onChangeDistance({ value: parseFloat(`${integer}.${decimal}`), unit });
+    onChangeDistance(parseFloat(`${integer}.${decimal}`) * 1000);
   }, [integer, decimal, unit]);
 
   return (
     <View style={{ flexDirection: "row" }}>
-      <Text
-        style={{
-          color: Colors.Gray,
-          fontSize: 16,
-          textAlign: "center",
-        }}
-      >
-        Running a distance of
-      </Text>
       <TextInput
         value={integer.toString()}
         keyboardType="number-pad"
         selectTextOnFocus
-        onChangeText={(text) => setInteger(parseInt(text, 10))}
+        style={styles.inputText}
+        onChangeText={(text) => setInteger(parseIntOrZero(text))}
       />
-      <Text>.</Text>
+      <Text style={[styles.inputSupportText, { marginHorizontal: 3 }]}>.</Text>
       <TextInput
         value={decimal.toString()}
         keyboardType="number-pad"
         selectTextOnFocus
-        onChangeText={(text) => setDecimal(parseInt(text, 10))}
+        style={styles.inputText}
+        onChangeText={(text) => setDecimal(parseIntOrZero(text))}
       />
-      <Text>{unit}</Text>
+
+      {/* todo: change unit */}
+      <Text style={styles.inputText}>{unit}</Text>
     </View>
   );
 }
@@ -111,53 +118,71 @@ function TimeInput({ onChangeTime }) {
 
   return (
     <View style={{ flexDirection: "row" }}>
-      <Text
-        style={{
-          color: Colors.Gray,
-          fontSize: 16,
-          textAlign: "center",
-        }}
-      >
-        in
-      </Text>
       <TextInput
         value={hours.toString()}
         keyboardType="number-pad"
         selectTextOnFocus
         onChangeText={(text) => setHours(parseInt(text, 10))}
-        style={{ marginHorizontal: 5, fontSize: 16 }}
+        style={styles.inputText}
       />
-      <Text style={{ fontSize: 16, color: Colors.Gray }}>h</Text>
+      <Text style={styles.inputSupportText}>h</Text>
       <TextInput
         value={minutes.toString()}
         keyboardType="number-pad"
         selectTextOnFocus
         onChangeText={(text) => setMinutes(parseInt(text, 10))}
-        style={{ marginHorizontal: 5, fontSize: 16 }}
+        style={styles.inputText}
       />
-      <Text style={{ fontSize: 16, color: Colors.Gray }}>min</Text>
+      <Text style={styles.inputSupportText}>min</Text>
       <TextInput
         value={seconds.toString()}
         keyboardType="number-pad"
         selectTextOnFocus
         onChangeText={(text) => setSeconds(parseInt(text, 10))}
-        style={{ marginHorizontal: 5, fontSize: 16 }}
+        style={styles.inputText}
       />
-      <Text style={{ fontSize: 16, color: Colors.Gray }}>sec</Text>
+      <Text style={styles.inputSupportText}>sec</Text>
     </View>
   );
 }
 
-function Pace({ distance, time }) {
-  return null;
+function Pace({ distanceMeters, timeSeconds, unit, onChangeUnit }) {
+  const pace = React.useMemo(() => {
+    if (distanceMeters === 0 || timeSeconds === 0) {
+      return "∞";
+    }
+
+    let secondsPerUnit;
+    if (unit === "km") {
+      const distanceKm = distanceMeters / 1000.0;
+      secondsPerUnit = timeSeconds / distanceKm;
+    } else if (unit === "mi") {
+      secondsPerUnit = timeSeconds / (distanceMeters * MILES_PER_METER);
+    }
+
+    return formatDuration(secondsPerUnit * 1000);
+  }, [distanceMeters, timeSeconds, unit]);
+
+
+  // todo: split minutes/seconds out so they can be changed to update the time as well
+
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Text style={styles.inputText}>{pace}</Text>
+      <View style={{ marginHorizontal: 3 }} />
+      <Text style={styles.inputText}>min/{unit}</Text>
+    </View>
+  );
 }
 
 function Calculator() {
   let [fontsLoaded] = useFonts({
     OpenSans_400Regular,
   });
-  const [distanceMeters, setDistanceMeters] = useState();
-  const [timeSeconds, setTimeSeconds] = useState();
+
+  const [unit, setUnit] = useState<DistanceUnit>("km");
+  const [distanceMeters, setDistanceMeters] = useState<number>();
+  const [timeSeconds, setTimeSeconds] = useState<number>();
   const insets = useSafeAreaInsets();
 
   if (!fontsLoaded) {
@@ -167,6 +192,7 @@ function Calculator() {
   return (
     <ScrollView
       style={{ flex: 1 }}
+      keyboardDismissMode="on-drag"
       contentContainerStyle={{
         paddingTop: insets.top,
         alignItems: "center",
@@ -175,21 +201,28 @@ function Calculator() {
       <Logo />
       <Introduction />
       <DistanceInput
-        onChangeDistance={(value) => {
-          setDistanceMeters(JSON.stringify(value));
+        unit={unit}
+        onChangeUnit={(newUnit) => setUnit(newUnit)}
+        onChangeDistance={(distance) => {
+          setDistanceMeters(distance);
         }}
       />
 
       <TimeInput
         onChangeTime={(value) => {
-          setTimeSeconds(value);
+          setTimeSeconds(parseIntOrZero(value));
         }}
       />
 
-      {distanceMeters && timeSeconds ? <Pace distanceMeters={distanceMeters} timeSeconds={timeSeconds} /> : null}
-
-      <Text>{distanceMeters}</Text>
-      <Text>{timeSeconds}</Text>
+      {typeof distanceMeters !== "undefined" &&
+      typeof timeSeconds !== "undefined" ? (
+        <Pace
+          distanceMeters={distanceMeters}
+          timeSeconds={timeSeconds}
+          unit={unit}
+          onChangeUnit={(newUnit) => setUnit(newUnit)}
+        />
+      ) : null}
 
       <StatusBar style="auto" />
     </ScrollView>
@@ -209,5 +242,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  inputText: {
+    color: Colors.Blue,
+    fontSize: FONT_SIZE,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    fontWeight: "600",
+  },
+  inputSupportText: {
+    fontWeight: "600",
+    color: "#000",
+    fontSize: FONT_SIZE,
+    marginHorizontal: 10,
   },
 });
